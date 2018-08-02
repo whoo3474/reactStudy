ba956139fe2dd2ba995d3e27c28d78ad5db53bf9
ErickRyu <rkggmdii@gmail.com>
2018-08-02 20:42:44 +0900

    Draw plus, minus buttons and value

--- sungjin/180802_study/src/App.js
+++ sungjin/180802_study/src/App.js
@@ -3,9 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    num1: 0
+  }
   render() {
     return (
       <div className="App">
+        <button>+</button>
+        <div>value : {this.state.num1}</div>
+        <button>-</button>
       </div>
     );
   }
