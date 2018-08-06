b5dae36ef02e580c0e25d2bafae2ee7457a2eb5b
ErickRyu <rkggmdii@gmail.com>
2018-08-06 15:26:23 +0900

    Create PhoneForm component and replace App to App2

---
+++ sungjin/hello-react/src/App2.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
+
+
+class App2 extends Component {
+  render() {
+    return (
+      <div>
+        <PhoneForm />
+      </div>
+    );
+  }
+}
+
+export default App2;
