512c49f84c8b7f99d1842a013fbfced998782da4
ErickRyu <rkggmdii@gmail.com>
2018-08-06 16:14:40 +0900

    Add id,name,phone to information array

--- sungjin/hello-react/src/App2.js
+++ sungjin/hello-react/src/App2.js
@@ -3,15 +3,35 @@ import PhoneForm from './components/PhoneForm';
 
 
 class App2 extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민중',
+        phone: '010-111-1111'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-222-2222'
+      }
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
   }
   render() {
+    const {information } = this.state;
     return (
       <div>
         <PhoneForm 
           onCreate={this.handleCreate}
         />
+        {JSON.stringify(information)}
       </div>
     );
   }
