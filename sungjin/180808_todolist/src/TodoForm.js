9c31194d5eb105fb6b74936fa8048eba2ace9578
ErickRyu <rkggmdii@gmail.com>
2018-08-14 23:32:43 +0900

    Create 180808 todolist project

---
+++ sungjin/180808_todolist/src/TodoForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component {
+  render() {
+    return (
+      <div><input /><button>버튼</button></div>
+    );
+  }
+}
+
+export default TodoForm;
---
+++ sungjin/180808_todolist/src/TodoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Todo from './Todo';
+
+class TodoList extends Component {
+  render() {
+    return (
+			<div>
+			list
+			<Todo />
+			</div>
+    );
+  }
+}
+
+export default TodoList;
