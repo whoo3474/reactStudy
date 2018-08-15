b2a155b54973a980daddf18ee45e787bf446745f
ErickRyu <rkggmdii@gmail.com>
2018-08-15 13:18:49 +0900

    Add title state

--- sungjin/180808_todolist/src/App.js
+++ sungjin/180808_todolist/src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import TodoForm from './TodoForm';
 
 class App extends Component {
+  state = {
+    title : 'title',
+  }
   render() {
     return (
       <div>
         <TodoForm />
+        <p>{this.state.title}</p>
       </div>
     );
   }
