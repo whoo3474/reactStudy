55aa62f83ab55e7b480ad80e1e3585d6edde156f
kjw6825 <baramboy1@naver.com>
2018-08-23 15:47:05 +0900

    14-2. 멀티카운터 만들기

---
+++ KimJaeWook/Velopert/14. Redux 정복하기/2. 멀티카운터 만들기/src/containers/CounterListContainer.js
@@ -0,0 +1,19 @@
+import CounterList from '../components/CounterList';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
+import { getRandomColor } from '../util';
+
+const mapStateToProps = (state) => ({
+    counters: state.counters
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    onIncrement: (index) => dispatch(actions.increment(index)),
+    onDecrement: (index) => dispatch(actions.decrement(index)),
+    onSetColor: (index) => {
+        const color = getRandomColor();
+        dispatch(actions.setColor(index, color));
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterList);
